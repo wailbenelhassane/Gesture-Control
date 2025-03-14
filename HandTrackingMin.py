7ed195d158afb547fb41993ab06f819ba8caafd5
Walo <wailbenelhassane@gmail.com>
2025-03-14 13:41:24 +0000

    feat: Hand Tracking as module

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -4,6 +4,10 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+if not cap.isOpened():
+    print("No se puede acceder a la cámara")
+    exit()
+
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
@@ -19,14 +23,20 @@ while True:
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx, cy)
+                if id == 4:
+                    cv2.circle(img, (cx,cy), 15, (255,0,255), cv2.FILLED)
 
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     currTime = time.time()
     fps = 1/(currTime - prevTime)
     prevTime = currTime
 
-    cv2.putText(img, str(int(fps)),(10,80), cv2.FONT_HERSHEY_PLAIN, 3,(0,0,255), 3)
+    cv2.putText(img, str(int(fps)),(10,80), cv2.FONT_HERSHEY_PLAIN, 3,(255,0,0), 3)
 
     cv2.imshow("image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
---
+++ HandTrackingModule.py
@@ -0,0 +1,78 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class handDetector():
+
+    def __init__(self,mode = False, maxHands = 2, detectionConf = 0.5, trackConf = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionConf = float(detectionConf)
+        self.trackConf = float(trackConf)
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
+            static_image_mode=self.mode,
+            max_num_hands=self.maxHands,
+            min_detection_confidence=self.detectionConf,
+            min_tracking_confidence=self.trackConf
+        )
+        self.mpDraw = mp.solutions.drawing_utils
+
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        #print(results.multi_hand_landmarks)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 7, (0, 255, 0), cv2.FILLED)
+
+        return lmList
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("No se puede acceder a la cámara")
+        exit()
+
+    detector = handDetector()
+    prevTime = 0
+    currTime = 0
+
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+        currTime = time.time()
+        fps = 1 / (currTime - prevTime)
+        prevTime = currTime
+
+        cv2.putText(img, str(int(fps)), (10, 80), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+        cv2.imshow("image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    main()
